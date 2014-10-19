8204c5f96858bfb95ec59001dcc1101071851bd1
Geoff <geoff777@gmail.com>
2014-10-19 09:30:53 +0100

    corrected points of the rectangle but errors exist when calling rectangle points

--- rectangle.groovy
+++ rectangle.groovy
@@ -25,10 +25,40 @@ Point pt2 = new Point()
 pt2.x = Double.parseDouble(strx)
 pt2.y = Double.parseDouble(stry)
 
-Rectangle r1 = new Rectangle()
-r1.uppleft = pt1
-r1.downright = pt2
+if (pt1.x > pt2.x && pt1.y > pt2.y) {
+        Point pt3 = new Point()
+        pt3.x = pt2.x
+        pt3.y = pt1.y
+        Point pt4 = new Point()
+        pt4.x = pt1.x
+        pt4.y = pt2.y
+        Rectangle r1 = new Rectangle()
+        r1.uppleft = pt3
+        r1.downright = pt4
+        }
+else if (pt1.x > pt2.x && pt2.y > pt1.y) {   
+        Rectangle r1 = new Rectangle()
+        r1.uppleft = pt2
+        r1.downright = pt1
+        }
+else if (pt2.x > pt1.x && pt1.y > pt2.y) {
+        Rectangle r1 = new Rectangle()
+        r1.uppleft = pt1
+        r1.downright = pt2
+        }
+else if (pt2.x > pt1.x && pt2.y > pt1.y) {
+        Point pt3 = new Point()
+        pt3.x = pt1.x
+        pt3.y = pt2.y
+        Point pt4 = new Point()
+        pt4.x = pt2.x
+        pt4.y = pt1.y
+        Rectangle r1 = new Rectangle()
+        r1.uppleft = pt3
+        r1.downright = pt4
+        }
 
 double area = (pt2.x - pt1.x) * (pt2.y - pt1.y)
 
 println "area is: " + area
+println r1
