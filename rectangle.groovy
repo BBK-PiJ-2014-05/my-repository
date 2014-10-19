b40204d76f0c91583dc60bde7eaf5fee9b7f3e92
Geoff <geoff777@gmail.com>
2014-10-19 21:18:29 +0100

    added perimter but seems long-winded; couldn't get rectangle points to compute after if stmnts

--- rectangle.groovy
+++ rectangle.groovy
@@ -25,7 +25,9 @@ Point pt2 = new Point()
 pt2.x = Double.parseDouble(strx)
 pt2.y = Double.parseDouble(stry)
 
-if (pt1.x > pt2.x && pt1.y > pt2.y) {
+double area = 0
+
+if (pt1.x >= pt2.x && pt1.y >= pt2.y) {
         Point pt3 = new Point()
         pt3.x = pt2.x
         pt3.y = pt1.y
@@ -35,18 +37,29 @@ if (pt1.x > pt2.x && pt1.y > pt2.y) {
         Rectangle r1 = new Rectangle()
         r1.uppleft = pt3
         r1.downright = pt4
+        area = (pt4.x - pt3.x) * (pt3.y - pt4.y)
+        println "Area is: " + area
+        println "Perimeter is (upperleft,upperright,lowerright,lowerleft): " + r1.uppleft.x +"," + r1.uppleft.y + "," + r1.downright.x + "," + r1.uppleft.y + "," + r1.downright.x + "," + r1.downright.y + "," + r1.uppleft.x + "," + r1.downright.y
+         
         }
-else if (pt1.x > pt2.x && pt2.y > pt1.y) {   
+        
+else if (pt1.x >= pt2.x && pt2.y >= pt1.y) {   
         Rectangle r1 = new Rectangle()
         r1.uppleft = pt2
         r1.downright = pt1
+         area = (pt1.x - pt2.x) * (pt2.y - pt1.y)
+         println "Area is: " + area
+         println "Perimeter is (upperleft,upperright,lowerright,lowerleft): " + r1.uppleft.x +"," + r1.uppleft.y + "," + r1.downright.x + "," + r1.uppleft.y + "," + r1.downright.x + "," + r1.downright.y + "," + r1.uppleft.x + "," + r1.downright.y
         }
-else if (pt2.x > pt1.x && pt1.y > pt2.y) {
+else if (pt2.x >= pt1.x && pt1.y >= pt2.y) {
         Rectangle r1 = new Rectangle()
         r1.uppleft = pt1
         r1.downright = pt2
+        area = (pt2.x - pt1.x) * (pt1.y - pt2.y) 
+         println "Area is: " + area 
+         println "Perimeter is (upperleft,upperright,lowerright,lowerleft): " + r1.uppleft.x +"," + r1.uppleft.y + "," + r1.downright.x + "," + r1.uppleft.y + "," + r1.downright.x + "," + r1.downright.y + "," + r1.uppleft.x + "," + r1.downright.y
         }
-else if (pt2.x > pt1.x && pt2.y > pt1.y) {
+else    (pt2.x >= pt1.x && pt2.y >= pt1.y) {
         Point pt3 = new Point()
         pt3.x = pt1.x
         pt3.y = pt2.y
@@ -56,9 +69,8 @@ else if (pt2.x > pt1.x && pt2.y > pt1.y) {
         Rectangle r1 = new Rectangle()
         r1.uppleft = pt3
         r1.downright = pt4
+        area = (pt4.x - pt3.x) * (pt3.y - pt4.y)
+         println "Area is: " + area
+         println "Perimeter is (upperleft,upperright,lowerright,lowerleft): " + r1.uppleft.x +"," + r1.uppleft.y + "," + r1.downright.x + "," + r1.uppleft.y + "," + r1.downright.x + "," + r1.downright.y + "," + r1.uppleft.x + "," + r1.downright.y
         }
-
-double area = (pt2.x - pt1.x) * (pt2.y - pt1.y)
-
-println "area is: " + area
-println r1
+        
