6cfb87756d871c15dfc091a58de0fbd42ad093b9
Geoff <geoff777@gmail.com>
2014-10-27 22:50:31 +0000

    ok

--- mailserver.groovy
+++ mailserver.groovy
@@ -3,46 +3,48 @@ println "WOW! Email Server 1.0"
 //MAIL FROM:geoff777@gmail.com --length=28; beginemail=from pos 10
 
 boolean quit = false
-boolean cmdcheck = false
+boolean errorcheck = false
+char atsign
 while (!quit) {
-    while (!cmdcheck) {
+    while (!errorcheck) {
         print ">>> "
-        String cmd = System.console().readLine()
-        int len = cmd.length()
-        int x = 0
+        String str = System.console().readLine()
+        if (str == "QUIT") {
+            quit = true
+        }
+       
+        if (str.substring(0,10) != "MAIL FROM:") {
+            errorcheck = true
+            println "Error"
+        }  
+        int len = str.length()
         int c = 0
-        if (cmd.substring(0,10) == "MAIL FROM:") {       
-            while (x <= len - 11) {
-                char atsign = cmd.charAt(10 + x)
-                if (atsign == '@') {
-                    c++
-                }
-                    x++
- 
-            }
-            if (c == 1) {
-                cmdcheck = true
-            }
-            else {
-                print "invalid (too many @)"
-            }      
-            atsign = cmd.charAt(10)
+        int x = 0
+        while (x < len) {
+            atsign = str.charAt(x)
             if (atsign == '@') {
-                println "invalid entry, (@at pos 1)"
+                c++
             }
-            else {
-                cmdcheck = true
-            }
-             atsign = cmd.charAt(len-1)
-             if (atsign == '@') {
-                 println "invalid, @ at last"
-            }
-            else { cmdcheck = true
-            }
+            x++
+        }
+        if (c != 1) {
+            errorcheck = true
+        }
+        atsign = str.charAt(10)
+        if (atsign == '@') {
+            errorcheck = true
+        }
+        atsign = str.charAt(len-1)
+        if (atsign == '@') {
+            errorcheck = true
             
-         }    
-     
-     }
-     quit = true
-}           
-print "All entries valid"
+        }
+      
+    }    
+
+
+
+print "OK"
+
+
+}                          
