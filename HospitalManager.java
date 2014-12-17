0f3f174a982b60d30cc650d85c6b8ed693208abe
Geoff <geoff777@gmail.com>
2014-12-17 13:39:35 +0000

    revised patient and hm

--- HospitalManager.java
+++ HospitalManager.java
@@ -1,26 +1,33 @@
 public class HospitalManager {
-	private Patient PatientListStart = null;
 
-	public static void main(String[] args) {
-		HospitalManager hm = new HospitalManager();
+private Patient patientListStart = null;
 
-		String name = args[0];
-		int age = Integer.parseInt(args[1]);
-		String illness = args[2];
+private void launch() {
+	Patient p1 = new Patient("Ben",58, "Thyphoid");
+	patientListStart = p1;
+	//patientListStart.addPatient(p1);
+	Patient p2 = new Patient("Sean", 52, "Cough");
+	patientListStart.addPatient(p2);
+	Patient p3 = new Patient("Sam", 40, "Murculie");
+	patientListStart.addPatient(p3);
+	Patient p4 = new Patient("Geoff",40,"Shingles");
+	patientListStart.addPatient(p4);
+	Patient p5 = new Patient("Theo",30,"Measles");
+	patientListStart.addPatient(p5);
+	Patient p6 = new Patient("Jenna",28,"Malaria");
+	patientListStart.addPatient(p6);
 
-		Patient patient1 = new Patient(name,age,illness);
-		patient1.launch();
+	patientListStart.deletePatient(p2);
 
-	}
-		private void launch() {
-
-
-			System.out.println(patient1.getName());
-
-		}
+	System.out.println("List length: " + patientListStart.getListLength());
+	patientListStart.printList();
+}
 
 
+public static void main(String[] args) {
+	HospitalManager hm = new HospitalManager();
+	hm.launch();
 
 
-
+}
 }
--- Patient.java
+++ Patient.java
@@ -3,34 +3,84 @@ public class Patient {
 private String name;
 private int age;
 private String illness;
-private Patient nextPatient = null;
+private Patient nextPatient;
+private Patient previousPatient;
+private static int listLength = 0;
 
-	public Patient(String name, int age, String illness) {
+
+	Patient(String name, int age, String illness) {
 		this.name = name;
 		this.age = age;
 		this.illness = illness;
+		this.nextPatient = null;
+		this.previousPatient = null;
 	}
 
 	public void addPatient(Patient newPatient) {
-			if (this.nextPatient == null) {
-				this.nextPatient = newPatient;
-			} else {
-				this.nextPatient.addPatient(newPatient);
-			}
+		if (this.nextPatient == null) {
+			this.nextPatient = newPatient;
+			newPatient.previousPatient = this;
+			//System.out.println("This.name: " + this.name);
+			//System.out.println("This.nextPatient.name: " + this.nextPatient.name);
+			//System.out.println("newPatient.previousPatient.name: " + newPatient.previousPatient.name);
+		} else {
+
+			this.nextPatient.addPatient(newPatient);
 		}
-		public String getName() {
-			return this.name;
+	}
+
+	public String getNextPatientName() {
+		return this.nextPatient.name;
+	}
+
+	public String getPreviousPatientName() {
+		return this.previousPatient.name;
+	}
+
+	public boolean deletePatient(Patient patient) {
+		if (this.nextPatient == null) {
+			return false;
+		} else if (this.nextPatient.name.equals(patient.name)) {
+			this.nextPatient = nextPatient.nextPatient;
+			this.nextPatient.previousPatient = this;
+
+			return true;
+		}
+		else { return this.nextPatient.deletePatient(patient);
+		}
+	}
+
+	public int getListLength() {
+		listLength++;
+		if (this.nextPatient == null) {
+			return listLength;
+		}
+		else {
+			return this.nextPatient.getListLength();
+
+		}
+	}
+
+	public void printList() {
+		if 	(this.previousPatient == null) {
+			System.out.println("Beginning of list>>");
+			System.out.println("this.name: " + this.name);
+			System.out.println("nextPatient.name: " + this.nextPatient.name);
+			this.nextPatient.printList();
 		}
 
+		else if	(this.nextPatient == null) {
+			System.out.println("this.name: " + this.name);
+			System.out.println("previousPatient.name: " + this.previousPatient.name);
+			System.out.println("<<end of list");
+		} else {
+			System.out.println("This.name: " + this.name);
+			System.out.println("nextPatient.name: " + this.nextPatient.name);
+			System.out.println("previousPatient.name: " + this.previousPatient.name);
+			this.nextPatient.printList();
+		}
+	}
 
-public static void main(String[]args) {
-	String name = args[0];
-	int age = Integer.parseInt(args[1]);
-	String illness = args[2];
-	Patient patient1 = new Patient(name,age,illness);
-	System.out.println(patient1.name);
-	Patient patient2 = new Patient("Harry",50,"Shingles");
-	patient1.addPatient(patient2);
-	System.out.println(patient1.nextPatient.name);
-}
+
+
 }
